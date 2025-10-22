989976aebfbfa4a0bb8aa58fd5ff36e91e7f1a86
Vincent Rein <155999069+vincentrein@users.noreply.github.com>
2025-10-22 17:08:25 +0200

    Implement in-place subtraction for Vector class (#3)
    Added method of class Point2D that allows subtraction in place (-=) of two 2D vectors
    
    Co-authored-by: Aaron Schöne <157137905+aaronsch0@users.noreply.github.com>

--- exercise2.py
+++ exercise2.py
@@ -14,6 +14,11 @@ class Point2D:
     def y(self) -> float:
         return self._coordinates[1]
 
+
+    def __isub__(self, other: Vector) -> None:
+        self._coordinates -= other
+        return self
+
     def __iadd__(self, other: Vector) -> None:
         self._coordinates += other
         return self
